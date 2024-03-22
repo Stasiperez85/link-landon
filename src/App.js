be839da4f96fb490e7d4a207dcbc059b1fcbd365
Anastasia Perez <Stasiperez85@gmail.com>
2024-03-21 22:26:39 -0600

    Added JSON to represent data and made components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Footer from './components/Footer.js'
+import Header from './components/Header.js'
+import Main from './components/Main'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-    
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React in less than a day like a rockstar
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Hotelinfo from "./HotelInfo";
+import Welcome from "./Welcome";
+
+const Main = () => {
+  return (
+    <main id="wrapper">
+      <Welcome />
+      <Hotelinfo />
+    </main>
+  );
+};
+
+export default Main;
